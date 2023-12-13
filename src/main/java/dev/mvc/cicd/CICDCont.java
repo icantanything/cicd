1b8b8ef4b354c568db57f18a729c96fcba305586
hyeonsu6 <bhshs0508@naver.com>
2023-12-13 12:21:11 +0900

    workflow 정상 테스트

--- src/main/java/dev/mvc/cicd/CICDCont.java
+++ src/main/java/dev/mvc/cicd/CICDCont.java
@@ -13,19 +13,25 @@ public class CICDCont {
   // http://localhost:9091/cicd?sw=-1
   // http://localhost:9091/cicd?sw=0
   @GetMapping("/cicd")
-  public String cicd(@RequestParam(defaultValue = "1") Integer sw) { // defaultValue = "1" -> 1
+  public String cicd(@RequestParam(defaultValue = "1") Integer sw) {
     String msg = "";
     
-    if (sw == -1) {
+    if (sw == 1) {
+      msg = "/cicd 호출됨, info 로그";
+      log.info(msg);
+    } else if (sw == -1) {
       msg = "/cide 호출됨, error 로그";
       log.error(msg);
     } else if (sw == 0) {
       msg = "/cide 호출됨, warn 로그";
       log.warn(msg);
-    }else if (sw == 1) {
-      msg = "/cicd 호출됨, info 로그";
+    } else if (sw == 2) {
+      msg = "/cide workflow 테스트";
       log.info(msg);
     }
+
+    
     return "<h3>" + msg + "</h3>";
   }
-}    
+}
+ 
